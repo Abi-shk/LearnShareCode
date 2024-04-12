3b9d972cc0c72ef7aed4f7e49cd91e959758f393
ABHISHEK K <abishkpk@gmail.com>
2024-04-12 14:12:17 +0530

    edited advanced chat page

--- src/components/advanced.jsx
+++ src/components/advanced.jsx
@@ -3,7 +3,7 @@ import { MultiChatSocket,MultiChatWindow,useMultiChatLogic } from "react-chat-en
 
 function Advanced({chatUser}) {
     const chatProps = useMultiChatLogic (
-        "6c22a910-f41d-4d78-9011-bda60465bf24",chatUser?.chatUser,chatUser?.chatSecret
+        "0838887b-b9d0-437c-bd9c-719bcf250e36",chatUser?.chatUser,chatUser?.chatSecret
       )
       return (
         <div style={{ height: "100vh" }}>
