d3c6c85a68761e5009a55d7ae8daa31ea0294393
ABHISHEK K <abishkpk@gmail.com>
2024-04-12 13:59:41 +0530

    edited chat id

--- src/components/chatData.jsx
+++ src/components/chatData.jsx
@@ -3,11 +3,13 @@ import React from 'react';
 import { PrettyChatWindow } from "react-chat-engine-pretty";
 
 function ChatData({chatUser}) {
+
+  console.log(chatUser)
     
      return(
       <div style={{ height: "100vh", width: "100vw" }}>
       <PrettyChatWindow
-        projectId={"6c22a910-f41d-4d78-9011-bda60465bf24"}
+        projectId={"0838887b-b9d0-437c-bd9c-719bcf250e36"}
         username={chatUser?.chatUser}
         secret={chatUser?.chatUser}
       />
